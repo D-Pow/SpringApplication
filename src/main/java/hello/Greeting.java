ee2e9e1e7e3e4d9d7407b5613eb6c777dc50b953
D-Pow <djp460@nyu.edu>
2019-12-13 20:54:59 -0500

    Add base of Spring Boot application with sample endpoint

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,42 @@
+package hello;
+
+public class Greeting {
+    private long id;
+    private String name;
+    private String postBody;
+
+    public Greeting(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Greeting(long id, String name, String postBody) {
+        this.id = id;
+        this.name = name;
+        this.postBody = postBody;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPostBody() {
+        return postBody;
+    }
+
+    public void setPostBody(String postBody) {
+        this.postBody = postBody;
+    }
+}
