eabb7ccf37b39840d68d5cacbe9aa94b8bd7e6c7
D-Pow <djp460@nyu.edu>
2020-02-16 11:38:03 -0500

    Add custom query to get all Employee entries by name

--- src/main/java/org/dpow/backend/persistence/EmployeeRepository.java
+++ src/main/java/org/dpow/backend/persistence/EmployeeRepository.java
@@ -1,6 +1,16 @@
 package org.dpow.backend.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @Query(
+            "SELECT employee " +
+            "FROM Employee employee " +
+            "WHERE LOWER(employee.name) LIKE %:name%"
+    )
+    List<Employee> getEmployeesByName(@Param("name") String name);
 }
