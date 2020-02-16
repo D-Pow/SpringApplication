5a8e2f91e01a94f67be58f1cd6af03d617276163
D-Pow <djp460@nyu.edu>
2020-02-16 11:47:04 -0500

    Add autowired EmployeeRepository and search employees by name in GET request
    This shows a minimal example of how Spring will configure a JpaRepository bean automatically with '@Autowire' and includes a basic example for querying the database upon GET request. Also includes a fallback if an employee with that name isn't found

--- src/main/java/org/dpow/backend/api/GreetingController.java
+++ src/main/java/org/dpow/backend/api/GreetingController.java
@@ -1,17 +1,32 @@
 package org.dpow.backend.api;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.dpow.backend.persistence.Employee;
+import org.dpow.backend.persistence.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        List<Employee> employeesWithName = employeeRepository.getEmployeesByName(name.toLowerCase());
+
+        if (employeesWithName.size() == 0) {
+            return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        }
+
+        Employee firstEmployee = employeesWithName.get(0);
+
+        return new Greeting(firstEmployee.getId(), firstEmployee.getName(), firstEmployee.getRole());
     }
 
     @PostMapping("/greeting")
